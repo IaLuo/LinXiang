f5c2a78fe47a36934ee7f9c33619ac8ca4c0eb1f
夏落 <xialuo@hugangtekimacbook-pro.local>
2016-04-24 16:58:40 +0800

    新建工程  －夏

---
+++ KnowledgeDemo/KnowledgeDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KnowledgeDemo
+//
+//  Created by 夏落 on 16/4/24.
+//  Copyright © 2016年 夏落. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        print("林翔")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
