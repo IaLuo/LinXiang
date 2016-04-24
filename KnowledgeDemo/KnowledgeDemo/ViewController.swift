dd2fe89ee01da30b9870ebd88eac43ae49ba5a5b
夏落 <xialuo@hugangtekimacbook-pro.local>
2016-04-24 18:46:42 +0800

    泛型比较  －夏

--- KnowledgeDemo/KnowledgeDemo/ViewController.swift
+++ KnowledgeDemo/KnowledgeDemo/ViewController.swift
@@ -13,13 +13,38 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        compareValue()
         
-        print("林翔")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    //比较大小
+    func compareValue() {
+        
+        let maxInt = maxValue(2, rhs: 3)
+        print("maxInt ==== \(maxInt)")
+        
+        let maxFloat = maxValue(7.4, rhs: 4.7)
+        print("maxFloat ==== \(maxFloat)")
+        
+        let maxStr = maxValue("defg", rhs: "hv")
+        print("maxStr ==== \(maxStr)")
+        
+    }
+    
+    //泛型比较
+    func maxValue<T:Comparable>(lhs:T, rhs:T) -> T {
+        if lhs > rhs {
+            return lhs
+        }
+        return rhs
+    }
+    
+    
+    
 
 
 }
